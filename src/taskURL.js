283560203bc2e5f4ddb77d87da64734daf279e9c
Bob Cunningham <bobchm@gmail.com>
2022-08-14 11:53:24 -0400

    point at server on Heroku

--- src/taskURL.js
+++ src/taskURL.js
@@ -1,3 +1,4 @@
 export default function taskURL(suffix) {
-    return "http://localhost:5000/" + suffix;
+    //    return "http://localhost:5000/" + suffix;
+    return "https://enigmatic-lowlands-53131.herokuapp.com/" + suffix;
 }
