6cebd0a79cd07711fcc3f06eba9a3c2c951357f9
Bob Cunningham <bobchm@gmail.com>
2022-09-12 16:06:01 -0400

    move to Railway.io instead of Heroku

--- src/utils/taskURL.js
+++ src/utils/taskURL.js
@@ -1,6 +1,7 @@
 export default function taskURL(suffix) {
     //    return "http://localhost:5000/" + suffix;
-    var url = "https://bobs-my-work-server.herokuapp.com/" + suffix;
+    // var url = "https://bobs-my-work-server.herokuapp.com/" + suffix;
+    var url = "https://web-production-ec09.up.railway.app/" + suffix;
     console.log(url);
     return url;
 }
