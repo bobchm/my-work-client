2568d89fd03411a6320d99838888ce68c815b7fc
Bob Cunningham <bobchm@gmail.com>
2022-11-01 08:08:06 -0400

    move back to Railway for the server

--- src/utils/taskURL.js
+++ src/utils/taskURL.js
@@ -1,7 +1,8 @@
 export default function taskURL(suffix) {
     //    return "http://localhost:5000/" + suffix;
     // var url = "https://bobs-my-work-server.herokuapp.com/" + suffix;
-    var url = "https://my-work-server.onrender.com/" + suffix;
+    // var url = "https://my-work-server.onrender.com/" + suffix;
+    var url = "https://web-production-ec09.up.railway.app/" + suffix;
     console.log(url);
     return url;
 }
